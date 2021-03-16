1629f8244bbcf246f5670af468fa41c74afbc6ed
Anand Singh1 <AnandSingh@fsoft4pdc.com>
2021-03-16 16:23:45 +0530

    added register page

---
+++ src/test/java/Pages/Register.java
@@ -0,0 +1,15 @@
+package Pages;
+
+public class Register {
+	
+	public void register(String name, String password){
+		System.out.println(" git test");
+	}
+
+	public static void main(String[] args) {
+	
+		
+
+	}
+
+}
