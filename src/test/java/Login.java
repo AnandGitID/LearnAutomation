0c95b48d94284056696b7ba95e1192a392f1bb82
AnandGitID <59915983+AnandGitID@users.noreply.github.com>
2021-03-16 17:03:10 +0530

    Create Login.java

---
+++ src/test/java/Login.java
@@ -0,0 +1,20 @@
+  
+public class Login {
+	
+	private By login=By.id("login");
+	
+	public void login(String name, String password){
+		System.out.println(" git test");
+	}
+
+	public static void main(String[] args) {
+	
+	
+
+	}
+	
+	public void clickOnLogin(){
+		System.out.println("click on register" + login);
+	}
+
+}
